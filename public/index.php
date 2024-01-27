26781a266a3c2d1abe3810c907cfd4ca0e1aadd9
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2024-01-27 16:43:33 -0600

    add routes

--- public/index.php
+++ public/index.php
@@ -1,4 +1,21 @@
 <?php
 require('../helpers.php');
+require __DIR__ . '/../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-loadView('home');
+$rootDir = $_SERVER['ROOT'];
+$uri = $_SERVER['REQUEST_URI'];
+
+$routes = [
+    "$rootDir/" => "controllers/home.php",
+    "$rootDir/listings" => "controllers/listings/index.php",
+    "$rootDir/listings/create" => "controllers/listings/create.php",
+    "$rootDir/404" => "controllers/error/404.php",
+];
+
+if (array_key_exists($uri, $routes)) {
+    require getBasePath($routes[$uri]);
+} else {
+    require getBasePath($routes['404']);
+}
